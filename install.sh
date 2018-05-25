8a820aa731a593844997ad4bce2cc1f60576b6fc
Tokida Hideaki <tokihide@gmail.com>
2018-05-26 07:55:54 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -5,37 +5,44 @@ set -u
 # 実行場所のディレクトリを取得
 THIS_DIR=$(cd $(dirname $0); pwd)
 
+cat << END
+**************************************************
+DOTFILES SETUP START.
+**************************************************
+END
+
+
 cd $THIS_DIR
 git submodule init
 git submodule update
 
-echo "start setup..."
 for f in .??*; do
+    # 次の場合には処理をスキップ
     [ "$f" = ".git" ] && continue
     [ "$f" = ".gitconfig.local.template" ] && continue
     [ "$f" = ".gitmodules" ] && continue
     [ "$f" = ".config" ] && continue
 
+    # ユーザのカレントディレクトリにlinkをする
+    echo "-> ${f}"
     ln -snfv ~/dotfiles/"$f" ~/
 done
 
 echo "--sub: .config directory"
 cd .config
 for f in *; do
-    echo "$f"
-    ln -snfv ~/dotfiles/.config/"$f" ~/.config/
+    if [ -d ~/dotfiles/.config/${f} ];then
+    	echo "-> .config/${f}"
+        ln -snfv ~/dotfiles/.config/"$f" ~/.config/
+    fi
 done
 cd $THIS_DIR
 
-
-[ -e ~/.gitconfig.local ] || cp ~/dotfiles/.gitconfig.local.template ~/.gitconfig.local
-
+# [ -e ~/.gitconfig.local ] || cp ~/dotfiles/.gitconfig.local.template ~/.gitconfig.local
 
 
 cat << END
-
 **************************************************
 DOTFILES SETUP FINISHED! bye.
 **************************************************
-
 END
